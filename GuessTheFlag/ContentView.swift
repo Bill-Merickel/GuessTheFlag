e970148484bbf0ebc6841e249ad467fbf425bc71
Bill-Merickel <billmerickel@gmail.com>
2025-03-24 23:32:31 -0400

    Add a user score state property, incorrect flag answer alert message, and game reset logic after 8 questions

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var showingScore = false
+  @State private var showingFinalScore = false
   @State private var scoreTitle = ""
+  @State private var scoreMessage = ""
+  @State private var score = 0
+  @State private var questionsAsked = 0
 
   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
   @State private var correctAnswer = Int.random(in: 0...2)
@@ -50,7 +54,7 @@ struct ContentView: View {
         .clipShape(.rect(cornerRadius: 20))
         Spacer()
         Spacer()
-        Text("Score: ???")
+        Text("Score: \(score)")
           .foregroundStyle(.white)
           .font(.title.bold())
         Spacer()
@@ -60,23 +64,41 @@ struct ContentView: View {
     .alert(scoreTitle, isPresented: $showingScore) {
       Button("Continue", action: askQuestion)
     } message: {
-      Text("Your score is ???")
+      Text(scoreMessage)
+    }
+    .alert("Game Over", isPresented: $showingFinalScore) {
+      Button("Play Again", action: reset)
+    } message: {
+      Text("Your final score was \(score).")
     }
   }
   
   func flagTapped(_ number: Int) {
     if number == correctAnswer {
       scoreTitle = "Correct"
+      scoreMessage = "Nice job!"
+      score += 1
     } else {
       scoreTitle = "Wrong"
+      scoreMessage = "That's the flag of \(countries[number])."
     }
-    
-    showingScore = true
+    questionsAsked += 1
+    if questionsAsked == 8 {
+      showingFinalScore = true
+    } else {
+      showingScore = true
+    }
   }
   
   func askQuestion() {
     countries.shuffle()
     correctAnswer = Int.random(in: 0...2)
+  }
+  
+  func reset() {
+    questionsAsked = 0
+    score = 0
+    askQuestion()
   }
 }
 
